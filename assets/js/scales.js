14caff5144ca301aad0f8646c5b06af7dc81be52
sten-bone <49705989+sten-bone@users.noreply.github.com>
2021-08-25 13:02:14 -0500

    Add new scales and update layout

--- assets/js/scales.js
+++ assets/js/scales.js
@@ -446,6 +446,300 @@ $(document).ready(function () {
         });
     }
 
+    // Diminished Scales
+    function setUpDiminishedScales() {
+        $("#c-dim").click(function() {
+            removeHighlights();
+            $("#c-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+        });
+
+        $("#db-dim").click(function() {
+            removeHighlights();
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#gb-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#bb-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+        });
+
+        $("#d-dim").click(function() {
+            removeHighlights();
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+        });
+
+        $("#eb-dim").click(function() {
+            removeHighlights();
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+        });
+
+        $("#e-dim").click(function() {
+            removeHighlights();
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+            $("#e-3").addClass("highlighted");
+        });
+
+        $("#f-dim").click(function() {
+            removeHighlights();
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+            $("#e-3").addClass("highlighted");
+            $("#f-3").addClass("highlighted");
+        });
+
+        $("#gb-dim").click(function() {
+            removeHighlights();
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+            $("#f-3").addClass("highlighted");
+            $("#gb-3").addClass("highlighted");
+        });
+
+        $("#g-dim").click(function() {
+            removeHighlights();
+            $("#g-1").addClass("highlighted");
+            $("#a-1").addClass("highlighted");
+            $("#as-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+        });
+
+        $("#ab-dim").click(function() {
+            removeHighlights();
+            $("#gs-1").addClass("highlighted");
+            $("#as-1").addClass("highlighted");
+            $("#b-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+        });
+
+        $("#a-dim").click(function() {
+            removeHighlights();
+            $("#a-1").addClass("highlighted");
+            $("#b-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+        });
+
+        $("#bb-dim").click(function() {
+            removeHighlights();
+            $("#as-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+        });
+
+        $("#b-dim").click(function() {
+            removeHighlights();
+            $("#b-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+        });
+    }
+
+    // Augmented (Whole-Tone) Scales
+    function setUpAugmentedScales() {
+        $("#c-aug").click(function() {
+            removeHighlights();
+            $("#c-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+        });
+
+        $("#db-aug").click(function() {
+            removeHighlights();
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+        });
+
+        $("#d-aug").click(function() {
+            removeHighlights();
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+        });
+
+        $("#eb-aug").click(function() {
+            removeHighlights();
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+        });
+
+        $("#e-aug").click(function() {
+            removeHighlights();
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+            $("#e-3").addClass("highlighted");
+        });
+
+        $("#f-aug").click(function() {
+            removeHighlights();
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+            $("#f-3").addClass("highlighted");
+        });
+
+        $("#gb-aug").click(function() {
+            removeHighlights();
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+            $("#e-3").addClass("highlighted");
+            $("#fs-3").addClass("highlighted");
+        });
+
+        $("#g-aug").click(function() {
+            removeHighlights();
+            $("#g-1").addClass("highlighted");
+            $("#a-1").addClass("highlighted");
+            $("#b-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+        });
+
+        $("#ab-aug").click(function() {
+            removeHighlights();
+            $("#gs-1").addClass("highlighted");
+            $("#as-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+        });
+
+        $("#a-aug").click(function() {
+            removeHighlights();
+            $("#a-1").addClass("highlighted");
+            $("#b-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+        });
+
+        $("#bb-aug").click(function() {
+            removeHighlights();
+            $("#as-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+        });
+
+        $("#b-aug").click(function() {
+            removeHighlights();
+            $("#b-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+        });
+    }
+
     // Major Pentatonic Scales
     function setUpMajorPentatonicScales() {
         $("#c-maj-p").click(function() {
@@ -692,12 +986,150 @@ $(document).ready(function () {
         });
     }
 
+    // Blues Scales
+    function setUpBluesScales() {
+        $("#c-blues").click(function() {
+            removeHighlights();
+            $("#c-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+        });
+
+        $("#db-blues").click(function() {
+            removeHighlights();
+            $("#cs-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+        });
+
+        $("#d-blues").click(function() {
+            removeHighlights();
+            $("#d-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+        });
+
+        $("#eb-blues").click(function() {
+            removeHighlights();
+            $("#ds-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+        });
+
+        $("#e-blues").click(function() {
+            removeHighlights();
+            $("#e-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#d-3").addClass("highlighted");
+            $("#e-3").addClass("highlighted");
+        });
+
+        $("#f-blues").click(function() {
+            removeHighlights();
+            $("#f-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#ds-3").addClass("highlighted");
+            $("#f-3").addClass("highlighted");
+        });
+
+        $("#gb-blues").click(function() {
+            removeHighlights();
+            $("#fs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+            $("#c-3").addClass("highlighted");
+            $("#cs-3").addClass("highlighted");
+            $("#e-3").addClass("highlighted");
+            $("#fs-3").addClass("highlighted");
+        });
+
+        $("#g-blues").click(function() {
+            removeHighlights();
+            $("#g-1").addClass("highlighted");
+            $("#as-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+        });
+
+        $("#ab-blues").click(function() {
+            removeHighlights();
+            $("#gs-1").addClass("highlighted");
+            $("#b-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+        });
+
+        $("#a-blues").click(function() {
+            removeHighlights();
+            $("#a-1").addClass("highlighted");
+            $("#c-2").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#g-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+        });
+
+        $("#bb-blues").click(function() {
+            removeHighlights();
+            $("#as-1").addClass("highlighted");
+            $("#cs-2").addClass("highlighted");
+            $("#ds-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#gs-2").addClass("highlighted");
+            $("#as-2").addClass("highlighted");
+        });
+
+        $("#b-blues").click(function() {
+            removeHighlights();
+            $("#b-1").addClass("highlighted");
+            $("#d-2").addClass("highlighted");
+            $("#e-2").addClass("highlighted");
+            $("#f-2").addClass("highlighted");
+            $("#fs-2").addClass("highlighted");
+            $("#a-2").addClass("highlighted");
+            $("#b-2").addClass("highlighted");
+        });
+    }
+
     // Set up all scales
     setUpMajorScales();
     setUpNaturalMinorScales();
     setUpHarmonicMinorScales();
+    setUpDiminishedScales();
+    setUpAugmentedScales();
     setUpMajorPentatonicScales();
     setUpMinorPentatonicScales();
+    setUpBluesScales();
 
     // function to choose a scale at random
     function chooseRandomScale() {
